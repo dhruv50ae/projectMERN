476d3cf46f87d3b02b8c31a5693c0d61274ca84f
dhruv50ae <linkdp2012@gmail.com>
2022-05-26 23:05:51 +0100

    Added and connected to the backend

--- server/index.js
+++ server/index.js
@@ -2,3 +2,27 @@ import express from "express";
 import bodyParser from "body-Parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://FryostAysCyolyd:FryostAysCyolyd123@cluster0.4pzh2.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connection(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
